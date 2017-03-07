ff602405089643cbece9dc2b33b293e5c07ca3f4
Jonathan Rawling <jonathan@rawling.me>
2017-03-07 16:38:29 +0000

    Add project files.

---
+++ GraphBuilder.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GraphBuilder.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
