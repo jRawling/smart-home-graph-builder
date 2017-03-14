52fffc0f99f761d6fc903c7a356597b4d78a8bfc
Jonathan Rawling <jonathan@rawling.me>
2017-03-14 09:42:57 +0000

    More work.

---
+++ GraphBuilder.Ui/BrandManager.cs
@@ -0,0 +1,31 @@
+﻿using GraphBuilder.Ui.Entities;
+using GraphBuilder.Ui.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphBuilder.Ui
+{
+    public class BrandManager
+    {
+        public BrandManager()
+        {
+            BrandRepository brandRepository = new BrandRepository();
+            brandRepository.DeleteAll();
+            Brand amazon = brandRepository.Create(new Brand("Amazon"));
+            Brand belkin = brandRepository.Create(new Brand("Belkin"));
+            Brand britishGas = brandRepository.Create(new Brand("British Gas"));
+            Brand veho = brandRepository.Create(new Brand("Veho"));
+            Brand philips = brandRepository.Create(new Brand("Philips"));
+            Brand smartThings = brandRepository.Create(new Brand("SmartThings"));
+            Brand nest = brandRepository.Create(new Brand("Nest"));
+            Brand apple = brandRepository.Create(new Brand("Apple"));
+            Brand netatmo = brandRepository.Create(new Brand("Netatmo"));
+            Brand netgear = brandRepository.Create(new Brand("Netgear"));
+            Brand ring = brandRepository.Create(new Brand("Ring"));
+            Brand panasonic = brandRepository.Create(new Brand("Panasonic"));
+            Brand dlink = brandRepository.Create(new Brand("D-Link"));
+
+        }
+    }
+}
--- GraphBuilder.Ui/Program.cs
+++ GraphBuilder.Ui/Program.cs
@@ -60,17 +60,50 @@ class Program
         ProductRepository productRepository = new ProductRepository();
         productRepository.DeleteAll();
 
-        Console.WriteLine("- Amazon");
-        Standalone echo = productRepository.Create(new Standalone("Echo", amazon, 149.99, alexa, null)) as Standalone;
-        Standalone echoDot = productRepository.Create(new Standalone("Echo Dot", amazon, 49.99, alexa, null)) as Standalone;
+        Amazon(amazon, alexa, productRepository);
+        BritishGas(britishGas, alexa, hive, productRepository);
+        Philips(philips, alexa, hue, smartThingsApp, nestApp, homeKit, productRepository);
+        Veho(veho, vehoApp, productRepository);
+        SmartThings(smartThings, smartThingsApp, productRepository);
+        Nest(nest, alexa, nestApp, productRepository);
+        Netatmo(netatmo, alexa, homeKit, netatmoThermostatApp, netatmoSecurityApp, netatmoWeatherApp, productRepository);
+    }
 
-        Console.WriteLine("- British Gas");
-        Hub hiveHub = productRepository.Create(new Hub("Hub", britishGas, 79.99)) as Hub;
-        Accessory hiveThermostat = productRepository.Create(new Accessory("Thermostat", britishGas, 99, hive, new List<App>() { alexa }, hiveHub)) as Accessory;
-        Accessory hivePlug = productRepository.Create(new Accessory("Active Plug", britishGas, 39.99, hive, new List<App>() { alexa }, hiveHub)) as Accessory;
-        Accessory hiveLight = productRepository.Create(new Accessory("Active LED Smart Bulb", britishGas, 19.99, hive, new List<App>() { alexa }, hiveHub)) as Accessory;
-        productRepository.Create(new Bundle("Active Heating", britishGas, 179.99, new List<Product>() { hiveHub, hiveThermostat }));
+    private static void Netatmo(Brand netatmo, App alexa, App homeKit, App netatmoThermostatApp, App netatmoSecurityApp, App netatmoWeatherApp, ProductRepository productRepository)
+    {
+        Console.WriteLine("- Netatmo");
+        Standalone netatmoThermostat = productRepository.Create(new Standalone("Thermostat", netatmo, 139.99, netatmoThermostatApp, new List<App>() { homeKit })) as Standalone;
+        Standalone netatmoWeatherStation = productRepository.Create(new Standalone("Urban Weather Station", netatmo, 129.99, netatmoWeatherApp, new List<App>() { alexa })) as Standalone;
+        Standalone netatmoWelcomeCam = productRepository.Create(new Standalone("Welcome Cam", netatmo, 159.99, netatmoSecurityApp, null)) as Standalone;
+        Standalone netatmoPresence = productRepository.Create(new Standalone("Presence", netatmo, 249.99, netatmoSecurityApp, null)) as Standalone;
+    }
 
+    private static void Nest(Brand nest, App alexa, App nestApp, ProductRepository productRepository)
+    {
+        Console.WriteLine("- Nest");
+        Standalone nestThermostat = productRepository.Create(new Standalone("Learning Thermostat", nest, 199, nestApp, new List<App>() { alexa })) as Standalone;
+    }
+
+    private static void SmartThings(Brand smartThings, App smartThingsApp, ProductRepository productRepository)
+    {
+        Console.WriteLine("- SmartThings");
+        Hub smartThingsHub = productRepository.Create(new Hub("Hub", smartThings, 99)) as Hub;
+        Accessory smartThingsMultiSensor = productRepository.Create(new Accessory("Multi Sensor", smartThings, 29.99, smartThingsApp, null, smartThingsHub)) as Accessory;
+        Accessory smartThingsPresenceSensor = productRepository.Create(new Accessory("Presence Sensor", smartThings, 29.99, smartThingsApp, null, smartThingsHub)) as Accessory;
+        Accessory smartThingsPowerOutlet = productRepository.Create(new Accessory("Power Outlet", smartThings, 44.99, smartThingsApp, null, smartThingsHub)) as Accessory;
+        Accessory smartThingsMotionSensor = productRepository.Create(new Accessory("Motion Sensor", smartThings, 29.99, smartThingsApp, null, smartThingsHub)) as Accessory;
+        Accessory smartThingsmoistureSensor = productRepository.Create(new Accessory("Moisture Sensor", smartThings, 29.99, smartThingsApp, null, smartThingsHub)) as Accessory;
+        productRepository.Create(new Bundle("Starter Kit", smartThings, 199.99, new List<Product>() { smartThingsHub, smartThingsMotionSensor, smartThingsMultiSensor, smartThingsPowerOutlet, smartThingsPresenceSensor }));
+    }
+
+    private static void Veho(Brand veho, App vehoApp, ProductRepository productRepository)
+    {
+        Console.WriteLine("- Veho");
+        Standalone vehoBulb = productRepository.Create(new Standalone("Bluetooth Smart LED Light Bulb", veho, 29.99, vehoApp, null)) as Standalone;
+    }
+
+    private static void Philips(Brand philips, App alexa, App hue, App smartThingsApp, App nestApp, App homeKit, ProductRepository productRepository)
+    {
         Console.WriteLine("- Philips");
         Hub philipsHueBridge = productRepository.Create(new Hub("Bridge", philips, 49.99)) as Hub;
         Accessory hueWhite = productRepository.Create(new Accessory("Hue White", philips, 14.99, hue, new List<App>() { alexa, homeKit, nestApp, smartThingsApp }, philipsHueBridge)) as Accessory;
@@ -80,26 +113,22 @@ class Program
         productRepository.Create(new Bundle("Hue Colour Starter Kit", philips, 149.99, new List<Product>() { philipsHueBridge, hueColour }));
         productRepository.Create(new Bundle("Hue Colour Starter Kit with LightStrip", philips, 174.98, new List<Product>() { philipsHueBridge, hueColour }));
         productRepository.Create(new Bundle("Hue Colour Starter Kit", philips, 59.99, new List<Product>() { philipsHueBridge, hueWhite }));
+    }
 
-        Console.WriteLine("- Veho");
-        Standalone vehoBulb = productRepository.Create(new Standalone("Bluetooth Smart LED Light Bulb", veho, 29.99, vehoApp, null)) as Standalone;
+    private static void BritishGas(Brand britishGas, App alexa, App hive, ProductRepository productRepository)
+    {
+        Console.WriteLine("- British Gas");
+        Hub hiveHub = productRepository.Create(new Hub("Hub", britishGas, 79.99)) as Hub;
+        Accessory hiveThermostat = productRepository.Create(new Accessory("Thermostat", britishGas, 99, hive, new List<App>() { alexa }, hiveHub)) as Accessory;
+        Accessory hivePlug = productRepository.Create(new Accessory("Active Plug", britishGas, 39.99, hive, new List<App>() { alexa }, hiveHub)) as Accessory;
+        Accessory hiveLight = productRepository.Create(new Accessory("Active LED Smart Bulb", britishGas, 19.99, hive, new List<App>() { alexa }, hiveHub)) as Accessory;
+        productRepository.Create(new Bundle("Active Heating", britishGas, 179.99, new List<Product>() { hiveHub, hiveThermostat }));
+    }
 
-        Console.WriteLine("- SmartThings");
-        Hub smartThingsHub = productRepository.Create(new Hub("Hub", smartThings, 99)) as Hub;
-        Accessory smartThingsMultiSensor = productRepository.Create(new Accessory("Multi Sensor", smartThings, 29.99, smartThingsApp, null, smartThingsHub)) as Accessory;
-        Accessory smartThingsPresenceSensor = productRepository.Create(new Accessory("Presence Sensor", smartThings, 29.99, smartThingsApp, null, smartThingsHub)) as Accessory;
-        Accessory smartThingsPowerOutlet = productRepository.Create(new Accessory("Power Outlet", smartThings, 44.99, smartThingsApp, null, smartThingsHub)) as Accessory;
-        Accessory smartThingsMotionSensor = productRepository.Create(new Accessory("Motion Sensor", smartThings, 29.99, smartThingsApp, null, smartThingsHub)) as Accessory;
-        Accessory smartThingsmoistureSensor = productRepository.Create(new Accessory("Moisture Sensor", smartThings, 29.99, smartThingsApp, null, smartThingsHub)) as Accessory;
-        productRepository.Create(new Bundle("Starter Kit", smartThings, 199.99, new List<Product>() { smartThingsHub, smartThingsMotionSensor, smartThingsMultiSensor, smartThingsPowerOutlet, smartThingsPresenceSensor }));
-
-        Console.WriteLine("- Nest");
-        Standalone nestThermostat = productRepository.Create(new Standalone("Learning Thermostat", nest, 199, nestApp, new List<App>() { alexa })) as Standalone;
-
-        Console.WriteLine("- Netatmo");
-        Standalone netatmoThermostat = productRepository.Create(new Standalone("Thermostat", netatmo, 139.99, netatmoThermostatApp, new List<App>() { homeKit })) as Standalone;
-        Standalone netatmoWeatherStation = productRepository.Create(new Standalone("Urban Weather Station", netatmo, 129.99, netatmoWeatherApp, new List<App>() { alexa })) as Standalone;
-        Standalone netatmoWelcomeCam = productRepository.Create(new Standalone("Welcome Cam", netatmo, 159.99, netatmoSecurityApp, null)) as Standalone;
-        Standalone netatmoPresence = productRepository.Create(new Standalone("Presence", netatmo, 249.99, netatmoSecurityApp, null)) as Standalone;
+    private static void Amazon(Brand amazon, App alexa, ProductRepository productRepository)
+    {
+        Console.WriteLine("- Amazon");
+        Standalone echo = productRepository.Create(new Standalone("Echo", amazon, 149.99, alexa, null)) as Standalone;
+        Standalone echoDot = productRepository.Create(new Standalone("Echo Dot", amazon, 49.99, alexa, null)) as Standalone;
     }
 }
